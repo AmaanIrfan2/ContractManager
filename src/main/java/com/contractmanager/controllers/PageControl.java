6f744f2e563c6ad270060108fe28e7a09e24f85c
Amaan Irfan <amaanirfan2@gmail.com>
2024-10-23 16:14:15 +0530

    updates

--- src/main/java/com/contractmanager/controllers/PageControl.java
+++ src/main/java/com/contractmanager/controllers/PageControl.java
@@ -11,4 +11,35 @@ public class PageControl {
         System.out.println("Home page handler");
         return "home";
     }
+
+    @RequestMapping("/about")
+    public String about(){
+        System.out.println("This is about page");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String services(){
+        System.out.println("This is the services page");
+        return "services";
+    }
+
+    @RequestMapping("/base")
+    public String base(){
+        System.out.println("A base page");
+        return "base";
+    }
+
+    @RequestMapping("/login")
+    public String base(){
+        System.out.println("A login page");
+        return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String base(){
+        System.out.println("A signup page");
+        return "signup";
+    }
 }
+
